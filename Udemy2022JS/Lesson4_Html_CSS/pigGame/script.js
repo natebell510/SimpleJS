0a180d4ae77bd0861fc6d8384124ef49a7ddf612
natebell510 <93531616+natebell510@users.noreply.github.com>
2022-10-05 09:35:04 -0600

    basic JS part 3

--- Udemy2022JS/Lesson4_Html_CSS/pigGame/script.js
+++ Udemy2022JS/Lesson4_Html_CSS/pigGame/script.js
@@ -1,4 +1,11 @@
 'use strict';
+//selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+
+
+score0El.textContent = 0;
+score1El.textContent = 0;
 
 
 
@@ -21,4 +28,6 @@
 
 
 
-// video 3 3:21:00
+
+
+// video 3 3:25:00
