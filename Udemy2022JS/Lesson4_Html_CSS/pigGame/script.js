d7a17060260f055c4cb9e8d0424eeab8826c2ee3
natebell510 <93531616+natebell510@users.noreply.github.com>
2022-10-17 13:57:16 -0600

    basic JS part 3

--- Udemy2022JS/Lesson4_Html_CSS/pigGame/script.js
+++ Udemy2022JS/Lesson4_Html_CSS/pigGame/script.js
@@ -3,10 +3,21 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 
+
+//starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+
+
+
+//3:30
 
 
 
