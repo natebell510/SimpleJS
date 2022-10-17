aa5f3c4a058da0dd3c0e7bab912afc7755e0a2a0
natebell510 <93531616+natebell510@users.noreply.github.com>
2022-10-17 14:19:09 -0600

    basic JS part 3

--- Udemy2022JS/Lesson4_Html_CSS/pigGame/script.js
+++ Udemy2022JS/Lesson4_Html_CSS/pigGame/script.js
@@ -2,6 +2,9 @@
 //selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -14,6 +17,26 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
+//rolling dice functionality
+
+btnRoll.addEventListener('click',function (){
+    //1.generate random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //2. display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //3. check for rolled 1: if true, switch to next player
+    if (dice !== 1){
+        currentScore += dice;
+        current0El.textContent = currentScore; //change later
+    }else{
+
+    }
+
+})
 
 
 
