c56a2e3ebd9bf63743b647612729d47d6a507236
natebell510 <93531616+natebell510@users.noreply.github.com>
2022-10-26 12:28:44 -0600

    basic JS part 3

---
+++ Udemy2022JS/Lesson4_Html_CSS/behindTheScenes/script.js
@@ -0,0 +1,17 @@
+'use strict';
+//principles of JS:
+//high level - hardware is not managed, low level ex. C - manage resources manually
+//garbage collector - cleaning memory that we do not need
+//Just-In-Time compilation, interpreted
+//multi paradigm - approach of structuring code, coding style
+//prototype based object-oriented
+//first class functions
+//dynamic - dynamically typed ex. let x = 23; let t = "Jonas";
+//single-threaded
+//non-blocking event loop
+
+//JS Engine executes JS code, every browser has its own, V8 -> Chrome NodeJS
+//Call Stack (references to objects) vs Heap (pool for objects)
+
+//parsing - split and save it to tree AST to check for error
+
