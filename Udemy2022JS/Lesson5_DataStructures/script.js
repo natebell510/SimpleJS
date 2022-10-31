6714284712ed483a628548ad1288434450d421ad
natebell510 <93531616+natebell510@users.noreply.github.com>
2022-10-31 12:27:52 -0600

    Data Structures

--- Udemy2022JS/Lesson5_DataStructures/script.js
+++ Udemy2022JS/Lesson5_DataStructures/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//1:36:00
 // Data needed for a later exercise
 const flights =
     '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -20,6 +20,10 @@ const restaurant = {
     orderPasta: function (ing1, ing2, ing3) {
         console.log(`Here is your pasta with ${ing1}, ${ing2}, ${ing3}`);
     },
+    orderPizza : function (mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
 
     openingHours: {
         thu: {
@@ -36,6 +40,29 @@ const restaurant = {
         },
     },
 };
+if(restaurant.orderPizza){
+    restaurant.orderPizza('onions','spinach');
+}
+console.log('--------------------------------');
+restaurant.orderPizza && restaurant.orderPizza('tomato','potato');
+
+
+console.log('--------------------------------');
+const guest1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guest1);
+const guest2 = restaurant.numGuests || 10;
+console.log(guest2);
+console.log('--------------------------------');
+if(restaurant.orderPizza()){
+    restaurant.orderPizza('onions','spinach');
+}
+
+
+console.log('--------------------------------');
+restaurant.orderPizza('mushrooms', 'onion','olives','spinach');
+restaurant.orderPizza('mushrooms'); //mushrooms,[]
+
+console.log('--------------------------------');
 const arr2 = [1, 2, ...[3, 4]];
 console.log(arr2); //spread operator because on right side of assign operator '='
 const [f, g, ...others] = [1, 2, 3, 4, 5];
