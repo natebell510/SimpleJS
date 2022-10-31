4651e8b87e0aac7d2b6835e339568291234df068
natebell510 <93531616+natebell510@users.noreply.github.com>
2022-10-31 10:37:42 -0600

    Data Structures

--- Udemy2022JS/Lesson5_DataStructures/script.js
+++ Udemy2022JS/Lesson5_DataStructures/script.js
@@ -2,28 +2,103 @@
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+    orderDelivery: function (starterIndex, mainIndex, time, address){
+        console.log(`Order received!${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be 
+        delivered to ${address} around ${time}`);
+    }
+    ,
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
     },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 };
+
+restaurant.orderDelivery({
+    time : '22.40',
+    address: 'Cranfield Link',
+    mainIndex : 2,
+    starterIndex : 2,
+});
+
+//Object destructuring
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+    name: restaurantName,
+    openingHours: hours,
+    categories: tags
+} = restaurant;
+console.log(restaurantName, hours, tags);
+const {menu = [], starterMenu : starters  = []} = restaurant;
+console.log(menu,starters);
+
+console.log('--------------------------------');
+let l = 111;
+let m = 999;
+const obj = {l: 23, m: 7, n: 14};
+({l, m} = obj);
+console.log(l,m);
+console.log('--------------------------------');
+//nested objects
+const {
+    fri:
+        {open: o,close: cl}
+} =  openingHours;
+console.log(o,cl);
+
+
+
+console.log('--------------------------------');
+//------------------
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+let [main, , secondary] = restaurant.categories; // ,skipped element, in array
+console.log(main, secondary);
+
+//switching variables
+//const temp = main;
+//main = secondary;
+//secondary = temp;
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+//*/*/*/*/*
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+//Nested destructuring
+const nested = [2, 4, [5, 6]];
+//const [i, , j] = nested;
+//console.log(i, j);
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+//default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
