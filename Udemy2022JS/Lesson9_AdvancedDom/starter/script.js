e19c243378e83a7f837432af9c9c6dffe60f2a74
natebell510 <93531616+natebell510@users.noreply.github.com>
2023-03-27 20:37:21 -0600

    mouse events, alerts

--- Udemy2022JS/Lesson9_AdvancedDom/starter/script.js
+++ Udemy2022JS/Lesson9_AdvancedDom/starter/script.js
@@ -36,6 +36,52 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//smooth scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click', function (e){
+  const s1coords = section1.getBoundingClientRect();
+  //scrolling
+ // window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+  /*
+  window.scrollTo({
+    left : s1coords.left + window.pageXOffset,
+    top : s1coords.top + window.pageYOffset,
+    behavior : 'smooth',
+  });
+   */
+
+  section1.scrollIntoView({
+    behavior : 'smooth'
+  });
+});
+
+const h1 = document.querySelector('h1');
+const alertH1 = function (e) {
+  alert('addEventListener: great! you are reading the heading.');
+  //listen to event just once
+  h1.removeEventListener('mouseenter',alertH1);
+};
+h1.addEventListener('mouseenter', alertH1);
+/*
+h1.addEventListener('mouseenter', function (e) {
+  alert('addEventListener: great! you are reading the heading.')
+});
+ */
+/*
+h1.onmouseenter = function (e) {
+  alert('addEventListener: great! you are reading the heading.')
+};
+ */
+
+
+
+
+
+
+
+
+/*
 //DOM, HTML collection
 //console.log(document.documentElement);
 //const allSections = document.querySelectorAll('.section');
@@ -61,6 +107,37 @@ document.querySelector('.btn--close-cookie').addEventListener('click', function(
   //message.remove();
   message.parentElement.removeChild(message); //DOM traversing
 });
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+//console.log(getComputedStyle(message));
+//console.log(getComputedStyle(message).color);//rgb(187, 187, 187)
+message.style.height = Number.parseFloat(getComputedStyle(message).height + 10) + 40 +'px';
+document.documentElement.style.setProperty('--color-primary','orangered');
+
+//Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.src);
+//logo.alt = 'Beautiful logo';
+logo.setAttribute('company','Bankist');
+console.log(logo.getAttribute('src'));
+const link = document.querySelector('.twitter-link')
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+
+//Data attributes
+//logo.dataset.versionNumber - used to store data in html code
+
+//classes
+logo.classList.add();
+logo.classList.remove();
+logo.classList.toggle('c');
+logo.classList.contains('');
+
+//do not use, will override all classes
+logo.className = 'jonnas';
+ */
 
 
 
